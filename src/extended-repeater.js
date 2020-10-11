a9c6274f06a54c4b9c09c15739cf9bc76e45087d
Oleg Zaytsev <zaytsevoi.90@gmail.com>
2020-10-11 23:35:25 +1200

    feat: add function repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,49 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  if (!options) return str;
+
+  const {
+    repeatTimes,
+    separator = "+",
+    addition,
+    additionRepeatTimes,
+    additionSeparator = "|",
+  } = options;
+
+  const repeater = [];
+
+  if (separator && addition) {
+    separator.toString();
+    addition.toString();
+  }
+
+  function getAddition(str) {
+    if (!repeatTimes && !additionRepeatTimes) return str;
+
+    const newStr = [];
+
+    for (let i = 0; i < additionRepeatTimes; i++) {
+      newStr.push(`${str}`);
+    }
+
+    return newStr.join(additionSeparator);
+  }
+
+  function getStr() {
+    if (!repeatTimes && !additionRepeatTimes) return;
+
+    for (let i = 0; i < repeatTimes - 1; i++) {
+      repeater.push(repeater[0]);
+    }
+  }
+
+  repeater.push(`${str}${getAddition(addition)}`);
+
+  if (!repeatTimes && !additionRepeatTimes) return repeater.join("");
+
+  getStr();
+
+  return repeater.join(separator);
 };
   
